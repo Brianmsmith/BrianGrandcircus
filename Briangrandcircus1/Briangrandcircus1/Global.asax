2a065be7100ba5a092ad793ecb84afd088a3a6b3
Brian M Smith <Brianmsmith777@gmail.com>
2014-06-25 10:38:34 -0500

    Project one comment
    Project number one

---
+++ Briangrandcircus1/Briangrandcircus1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Briangrandcircus1.MvcApplication" Language="C#" %>
---
+++ Briangrandcircus1/Briangrandcircus1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Briangrandcircus1.Startup))]
+namespace Briangrandcircus1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
